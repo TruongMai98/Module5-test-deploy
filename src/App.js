e760b3dc47285cf571047f8aa1ec33b73cd4698d
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2023-02-09 10:46:56 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn deploy reactjs - mike
         </a>
       </header>
     </div>
